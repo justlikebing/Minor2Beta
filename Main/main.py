7705aa04116f8d0866f70e1b8f7de2ede5f3c79d
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2024-09-06 15:35:52 +0530

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -2,6 +2,7 @@ from color_detection import color_func
 from imageCaptureWithCamera import openCameraImageClick
 from size_object import show_images
 
+
 # openCameraImageClick()
 show_images()
 color_func()
